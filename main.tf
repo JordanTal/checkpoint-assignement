e6817fd7c51984ac4f766dcc07907166fca1d0e9
Yarden_Tal <jordan23.tal@gmail.com>
2021-08-29 03:34:43 +0300

    first commit , ci conf is attached in dedicated file and not running with the main.tf because that ec2 is using dynamic external(public) IP and dns .

---
+++ main.tf
@@ -0,0 +1,59 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.27"
+    }
+  }
+
+  required_version = ">= 0.14.9"
+}
+
+provider "aws" {
+  profile = "default"
+  region  = "us-east-2"
+}
+
+resource "aws_security_group" "test_sg" {
+  name = "test_sg"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+    ingress {
+    from_port   = 5000
+    to_port     = 5000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "http"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # Allow outgoing traffic to anywhere.
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "app_server" {
+  ami           = "ami-0443305dabd4be2bc"
+  instance_type = "t2.micro"
+  associate_public_ip_address = true
+  user_data	= file("file.sh")
+  key_name         = "key1"  
+  security_groups = ["${aws_security_group.test_sg.name}"]
+  
+  tags = {
+    Name = "ExampleAppServerInstance"
+  }
+}
---
+++ readme.txt
@@ -0,0 +1,5 @@
+FYI - The ci configuration depends on the public DNS of the ec2 machine ehich is dynamic at the moment ( so drone ci is not initialized by main.tf run) , need to modify accordingly or set a static DNS / IP then run it on the instance. "
+
+when can set a static IP / DNS - 
+Please JUST include the drone configuration in the main.tf terraform file 
+for that just need to add the content of 'drone_configuration.txt' to 'file.sh'
